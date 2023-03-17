39923c1bd38d588e7be6400c53af6244deedeb2f
Fletcher Blight <fletcher.blight@gmail.com>
2023-03-18 00:54:30 +1000

    start on the book

---
+++ src/book/mod.rs
@@ -0,0 +1,7 @@
+//! # Learn Opengl - Graphics Programming, Rust Edition
+//! Learn modern OpenGL graphics programming in a step-by-step fashion
+//!
+//! Complete credit of tutorial source goes to "Joey de Vries" and the tutorials found here:
+//! <https://learnopengl.com/>
+//!
+//! This is intended as a faithful fan-edition, for the rust programming to use.
